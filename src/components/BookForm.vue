279c63922b1661c2760e889944104fe1240e19c6
anarraidoo <32473241+anarraidoo@users.noreply.github.com>
2022-07-09 21:34:05 -0400

    finished Step 2
    Enhancing bookform

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,7 +1,7 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+  <form v-on:submit.prevent="bookSubmit(bookData)" class="" action="#" method="post">
+    <input v-model=bookData.bookTitle type="text" name="title" value="" placeholder="Book Title">
+    <input v-model=bookData.bookAuthor type="text" name="author" value="" placeholder="Book Author">
     <div>
       <input v-model=bookData.finishedReading type="checkbox" name="finishedReading">
       <label for="finishedReading">Finished Reading</label>
@@ -20,19 +20,20 @@
 export default {
   name: "BookForm",
   props: ["books"],
+  bookData: {
+    bookTitle: "",
+    bookAuthor: "",
+    finishedReading: false,
+    ownership: []
+    },
   data() {
     return {
-      bookData: {
-        bookTitle: "",
-        bookAuthor: "",
-        finishedReading: false,
-        ownership: []
-        }
+      bookData;
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData.bookTitle, bookData.bookAuthor);
     }
   }
 };
